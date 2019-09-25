2febf659bda86132973d946a06d9d999555e58d0
Daniel Paul <daniel@identitysquare.ie>
2019-09-25 16:50:36 +0200

    Update 1. Variables.rb

--- 1. Variables.rb
+++ 1. Variables.rb
@@ -1,8 +1,17 @@
 puts 'Hello World!'
 
+
 greeting = 'Good morning!'
 puts greeting
 
-name = "Daniel"
-print "Welcome, #{name}”
 
+forecast = "sunny" # string
+temperature = 32 # number
+is_summer = true # boolean
+
+
+print "It's going to be #{forecast}, #{temperature} degrees outside today. "
+
+
+# output to the same line.
+print 'It\'s going to be a great day!'
