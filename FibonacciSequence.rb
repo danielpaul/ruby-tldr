688b55b9e92bed0692b2d6f93ec85c70cf21fd24
Daniel Paul <daniel@identitysquare.ie>
2019-10-01 14:14:22 +0100

    Update FibonacciSequence.rb

--- FibonacciSequence.rb
+++ FibonacciSequence.rb
@@ -1,8 +1,10 @@
 print "How many numbers in the Fibonacci sequence do you want? "
 numbers_to_print = gets.to_i
 
+# hard code the first two numbers of the sequence
 sequence = [0, 1]
 
+# i keeps track of array positions
 (1..numbers_to_print - 2).each do |i|
   sequence << sequence[i - 1] + sequence[i]
 end
