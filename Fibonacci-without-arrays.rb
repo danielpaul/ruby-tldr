404a8d1c16dea5f80fc629fa081c01e0d4d70f32
Daniel Paul <daniel@identitysquare.ie>
2019-10-01 17:31:41 +0100

    Update Fibonacci-without-arrays.rb

--- Fibonacci-without-arrays.rb
+++ Fibonacci-without-arrays.rb
@@ -1,14 +1,13 @@
 print "How many numbers in the Fibonacci sequence do you want? "
 numbers_to_print = gets.to_i
 
-# Alternative way of doing this without arrays
 last_number = 1
 last_last_number = 0
 
 puts 0
 puts 1
 
-numbers_to_print.times do 
+(numbers_to_print - 2).times do 
   new_number = last_last_number + last_number
   puts new_number
 
